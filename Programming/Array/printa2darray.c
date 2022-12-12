afcc4454d1643ae7968561d9be3e0331507a8ad2
Krishnendu Nandy <krishnendunandy43@gmail.com>
2022-12-12 21:05:17 +0530

    Done

---
+++ Programming/Array/printa2darray.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#define max 100
+int main()
+{
+    int m,n,i,j,a[max][max];
+    printf("\n Enter the number of rows and elements of an 2D array:");
+    scanf("%d %d", &m,&n);
+    for(i=0;i<m;i++)
+    {
+        printf("\n");
+        for(j=0;j<n;j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("\n The resultant Matrix is:-");
+    for(i=0;i<m;i++)
+    {
+        printf("\n");
+        for(j=0;j<n;j++)
+        {
+            printf("%d", a[i][j]);
+        }        
+    }
+    return 0;
+}
