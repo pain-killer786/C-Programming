06050939466c771c10414005e05a034e5741042e
Krishnendu Nandy <krishnendunandy43@gmail.com>
2022-12-24 13:39:08 +0530

    Done

---
+++ Programming/Data Structures And Algorithms/Elements.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void minMax(int arr[], int len, int *min, int *max)
+{
+    *min = *max = arr[0];
+    int i;
+    for(i=1 ; i<len ; i++)
+    {
+        if(arr[i]> *max)
+        {
+            *max = arr[i];
+        }
+        if(arr[i]< *min)
+        {
+            *min = arr[i];
+        }
+    }
+}
+
+int main()
+{
+    int a[]={23,4,21,98,987,45,32,10,123,986,50,3,4,5};
+    int min, max;
+    int len= sizeof(a) / sizeof(a[0]);
+    minMax(a , len , &min , &max);
+    printf("\n Minimum value in the array is : %d and Maximum value is: %d", min,max);
+    return 0;
+}
