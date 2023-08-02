5590de602be0eb93ada02a7615be75b15f09fe07
merikh786 <krishnendunandy43@gmail.com>
2023-08-02 13:44:03 +0530

    Done

--- Programming/Array/printa2darray.c
+++ Programming/Array/printa2darray.c
@@ -3,7 +3,7 @@
 int main()
 {
     int m,n,i,j,a[max][max];
-    printf("\n Enter the number of rows and elements of an 2D array:");
+    printf("\n Enter the number of rows and columns of an 2D array:");
     scanf("%d %d", &m,&n);
     for(i=0;i<m;i++)
     {
@@ -19,7 +19,7 @@ int main()
         printf("\n");
         for(j=0;j<n;j++)
         {
-            printf("%d", a[i][j]);
+            printf("%d ", a[i][j]);
         }        
     }
     return 0;
