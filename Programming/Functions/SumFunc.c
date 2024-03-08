b2b365c299fae328eaabe5e7a91a488fba49b698
Krishnendu Nandy <krishnendunandy43@gmail.com>
2024-03-09 01:39:53 +0530

    Added Info

--- Programming/Functions/SumFunc.c
+++ Programming/Functions/SumFunc.c
@@ -7,11 +7,11 @@ int main()
     int a,b;
     printf("Enter the values of a and b");
     scanf("%d %d", &a, &b);   
-    printf("The sum of teo number is %d",Sum(a,b));
+    printf("The sum of teo number is %d",Sum(a,b)); //Arguemeny / Actual Parameter
     return 0;
 }
 
-int Sum(int x, int y)
+int Sum(int x, int y) // Formal Parameter
 {
     int s=0;
     s=x+y;
